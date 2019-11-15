eb7dfcadd1b4aaa31b87bb514f63901bf0a8c3e7
lphung1 <loi_phung81@yahoo.com>
2019-11-15 14:27:47 -0500

    Initial commit

---
+++ src/main/java/com/skillstorm/data/TimeSheetDAO.java
@@ -0,0 +1,14 @@
+package com.skillstorm.data;
+
+public class TimeSheetDAO {
+	
+//	public User findByUsername() {};
+//	public List<TimeSheet> findTimeSheetsByUser(int id){};
+//	public Timesheet findTimeSheetById() {}
+//	public Timesheet save(Timesheet t) {}
+//	public Timesheet save(Timesheet t)[{}
+	
+	
+	
+
+}
