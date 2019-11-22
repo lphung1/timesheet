52000ad15323068b6d69c30361f89e6dab9588a2
lphung1 <loi_phung81@yahoo.com>
2019-11-21 19:35:01 -0500

    added some controllers

---
+++ src/main/java/com/skillstorm/controllers/TimesheetController.java
@@ -0,0 +1,21 @@
+package com.skillstorm.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.skillstorm.data.TimeSheetDAO;
+
+public class TimesheetController {
+
+	public static void getAllTimesheets(HttpServletRequest req, HttpServletResponse resp) {
+		
+		TimeSheetDAO t = new TimeSheetDAO();
+		
+		
+
+		
+	}
+	
+	
+
+}
